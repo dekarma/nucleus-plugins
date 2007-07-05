d9c4c1f50f82501ea8f257bb4854d937c1da88d8
edmondhui <edmondhui@81bba6dd-821a-0410-8ae2-735a8549c42a>
2007-07-05 14:16:48 +0000

    DlCounter v1.0

--- dlcounter/trunk/NP_DlCounter.php
+++ dlcounter/trunk/NP_DlCounter.php
@@ -15,24 +15,23 @@ History:
         - fix URI problem in skinVar
     0.7 - fix a bug with NP_Print
     0.8 - add getTableList()
-
-admun ToDO:
-- auto detect file path, added author id to table,
-- show file size
-- add admin menu
+    1.0 - show file size
+        - add admin menu
+        - auto file path detection
 */
 
 class NP_DlCounter extends NucleusPlugin {
     function getName() { return 'Download Counter'; }
     function getAuthor()  { return 'Drew Phillips | Rodrigo Moraes (conversion to plugin) | Edmond Hui (admun)'; }
     function getURL()  { return 'http://www.drew-phillips.com'; }
-    function getVersion() { return '0.8'; }
+    function getVersion() { return '1.0'; }
     function supportsFeature($SqlTablePrefix) { return 1; }
     function getDescription() { return 'A simple download counter.'; }
-    function getEventList() { return array('PreSkinParse', 'PreItem'); }
+    function getEventList() { return array('PreSkinParse', 'PreItem', 'QuickMenu'); }
 
     function install() {
         $this->createOption('usecookie', 'Use cookie to avoid counting more than once a file download made by the same person?', 'yesno', 'yes');
+        $this->createOption('showSize', 'Show file size', 'yesno', 'yes');
         sql_query('CREATE TABLE IF NOT EXISTS '.sql_table('plug_dl_count').' ( '.
             'file VARCHAR(255) NOT NULL, '.
             'count INT NOT NULL '.
@@ -44,9 +43,21 @@ class NP_DlCounter extends NucleusPlugin {
     function unInstall() {
         sql_query('DROP TABLE '.sql_table('plug_dl_count').';');
     }
-    
+
+    function hasAdminArea() { return 1; }
+
+    function event_QuickMenu(&$data) {
+      // only show when option enabled
+      global $member;
+      if (!($member->isLoggedIn() || !$member->isAdmin())) return;
+      array_push($data['options'],
+	array('title' => 'DlCounter',
+	  'url' => $this->getAdminURL(),
+	  'tooltip' => 'DlCounter Statistics'));
+    }
+
     function event_PreSkinParse(&$data) {
-        global $_COOKIE, $_GET, $CONF;
+        global $_COOKIE, $_GET, $CONF, $DIR_MEDIA;
         
         if(isset($_GET['file'])) {
             $file = $_GET['file'];
@@ -67,7 +78,7 @@ class NP_DlCounter extends NucleusPlugin {
             }
         
             //if the last two checks didnt get rid of a malicious user, this next one certainly will
-            if(!file_exists($_SERVER['DOCUMENT_ROOT'] . $_GET['file'])) {
+            if(!file_exists($DIR_MEDIA . $file)) {
                 echo "Specified file $file doesn't exist.";
                 exit;
             }
@@ -75,7 +86,7 @@ class NP_DlCounter extends NucleusPlugin {
             // cookie fix
             $cookie = str_replace(".", "_", $file);  
 
-            $query = "SELECT * FROM ".sql_table('plug_dl_count')." WHERE file = '$file'";
+            $query = "SELECT * FROM ".sql_table('plug_dl_count')." WHERE file = '".$file."'";
             $result = mysql_query($query);
             if(!$result) {
                 echo mysql_error();
@@ -84,7 +95,7 @@ class NP_DlCounter extends NucleusPlugin {
             
             if(mysql_num_rows($result) == 0) {
                 //first use of this file
-                $query = "INSERT INTO ".sql_table('plug_dl_count')." VALUES('$file', 1)";
+                $query = "INSERT INTO ".sql_table('plug_dl_count')." VALUES('".$file."', 1)";
                 $result = mysql_query($query);
                     if ($this->getOption('usecookie') == 'yes') {
                         setcookie("dl_" . $cookie, "set", time() + 60*60*24*365);
@@ -93,7 +104,7 @@ class NP_DlCounter extends NucleusPlugin {
             
             else {
                 if(!isset($_COOKIE['dl_' . $cookie]) || $this->getOption('usecookie') != 'yes') {
-                    $query = "UPDATE ".sql_table('plug_dl_count')." SET count = count + 1 WHERE file = '$file'";
+                    $query = "UPDATE ".sql_table('plug_dl_count')." SET count = count + 1 WHERE file = '".$file."'";
                     $result = mysql_query($query);
                     if ($this->getOption('usecookie') == 'yes') {
                         setcookie("dl_". $cookie, "set", time() + 60*60*24*365,$CONF['CookiePath'],$CONF['CookieDomain'],$CONF['CookieSecure']);
@@ -101,7 +112,7 @@ class NP_DlCounter extends NucleusPlugin {
                 }
             }
 
-            header("Location: http://" . $_SERVER['HTTP_HOST'] . $_GET['file']);
+            header("Location: " . $CONF['Self'] . "/media/" . $file);
         }
 
     } //event_PreSkinParse
@@ -115,19 +126,27 @@ class NP_DlCounter extends NucleusPlugin {
           $blog =& $manager->getBlog($CONF['DefaultBlog']);
         }
 
-        return "<a href=\"" . $blog->getURL() . "?file=". $matches[1] ."\">".$matches[2]."</a>";
+	$mem = MEMBER::createFromName($matches[1]);
+        return "<a href=\"" . $blog->getURL() . "?file=". $mem->getID() . "/".$matches[2]."\">".$matches[3]."</a>";
     }
 
     function event_PreItem($data) {
 	$this->currentItem = &$data["item"];
-	$this->currentItem->body = preg_replace_callback("#<\%DlCounter\((.*?)\,(.*?)\)\%\>#", array(&$this, 'replaceCallback'), $this->currentItem->body);
-	$this->currentItem->more = preg_replace_callback("#<\%DlCounter\((.*?)\,(.*?)\\)%\>#", array(&$this, 'replaceCallback'), $this->currentItem->more);
+
+	$this->currentItem->body = preg_replace_callback("#<\%DlCounter\((.*?)\,(.*?)\,(.*?)\)\%\>#", array(&$this, 'replaceCallback'), $this->currentItem->body);
+	$this->currentItem->more = preg_replace_callback("#<\%DlCounter\((.*?)\,(.*?)\,(.*?)\)\%\>#", array(&$this, 'replaceCallback'), $this->currentItem->more);
     }
 
-    function doSkinVar($skinType,$arg1='',$arg2='',$arg3='') {
-        global $manager, $blog, $CONF, $cnumber, $c, $mp;
+    function doSkinVar($skinType,$arg1='',$arg2='',$arg3='',$arg4='') {
+        global $manager, $blog, $CONF, $cnumber, $c, $mp, $DIR_MEDIA;
 
-        $query = "SELECT count FROM ".sql_table('plug_dl_count')." WHERE file = '$arg1'";
+	$mem = MEMBER::createFromName($arg1);
+
+        if ($this->getOption('showSize') == "yes") {
+	  $size = filesize($DIR_MEDIA.$mem->getID()."/".$arg2)." bytes, download ";
+	}
+
+        $query = "SELECT count FROM ".sql_table('plug_dl_count')." WHERE file = '".$mem->getID()."/".$arg2."'";
         $result = sql_query($query);
         if(mysql_num_rows($result) == 0) {
             $count = "0"; }
@@ -138,13 +157,14 @@ class NP_DlCounter extends NucleusPlugin {
         if(!$arg2) {
             $arg2 = "Download file"; }
         
-        if ($arg3 == '1') {
+        if ($arg4 == '1') {
             echo $count; }
         else {
-            echo "<a href=\"". $blog->getURL() . "?file=". $arg1 ."\">".$arg2."</a>";
+            echo "<a href=\"". $blog->getURL() . "?file=". $mem->getID() . "/" . $arg2 ."\">".$arg3."</a>";
             echo " (";
+	    echo $size;
             echo $count;
-            echo ")";
+            echo "x)";
         }
     } // doSkinVar
 } // class
