a1fb58dad7fbab945b1ddd24e793d5ec23019cb2
ftruscot <ftruscot@81bba6dd-821a-0410-8ae2-735a8549c42a>
2007-10-05 18:35:48 +0000

    NP_SiteList tag to 1.4.1. Bug fix when duplicate url entered.

---
+++ sitelist/tags/NP_SiteList-1.4.1/sitelist/pearcheck.php
@@ -0,0 +1,12 @@
+<?php
+// make sure we have the nucleus config info needed
+	include "../../../config.php";
+
+// define some variables
+    global $member;
+    if ($member->isAdmin()) {
+        if (strpos(strtolower(ini_get('include_path')), "pear")) echo "You have access to PEAR core files";
+        else echo "You seem to not have access to the PEAR core files. Sorry.";
+    }
+    else echo "Sorry. You are not logged in.";
+?>
