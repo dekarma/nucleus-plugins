0905f209d0b18c563eb46d55687d851860e0458d
edmondhui <edmondhui@81bba6dd-821a-0410-8ae2-735a8549c42a>
2008-02-19 19:51:57 +0000

    NP_CommentControl v0.7 II

---
+++ commentcontrol/trunk/commentcontrol/english.php
@@ -0,0 +1,28 @@
+<?php
+define('_WARNING_COMM_AWAIT',	'<h2>Comments Awaiting Approval</h2>');
+define('_WARNING_COMM_SAVED',	'<blockquote id="pending"><p><strong>Your comment was saved. We will review it ASAP.</strong></p></blockquote>');
+define('_WARNING_COMM_PEND',	'<blockquote id="pending"><p><strong>Comments must be approved before being published.</strong></p></blockquote>');
+define('_WARNING_RSS_AWAIT',	'RSS 2.0 of pending comments');
+define('_NO_COMM_PENDING',	'no comment pending<br />');
+define('_COMMENT_PENDING',	'comment(s) pending');
+define('_NEW_COMM_PENDING',	'<span class=\'r\'>[New comment pending!]</span>');
+
+define('_OPT_SHOW_IN_QMENU',	'Show in quick menu');
+define('_OPT_NAME_NO_COMMENT',	'Names of users that cannot place comments (separate with commas)');
+define('_OPT_DAYS_MOD_COMMENT',	'Moderate comments older than x days');
+define('_OPT_CHAR_ENCODING',	'Character encoding:');
+define('_OPT_RSS_PIC_DISPLAY',	'Picture to display:');
+define('_OPT_MOD_MEMBER',	'Should blog member\'s comment be controlled?');
+define('_OPT_Q_TOOL_TIPS',	'See the list of comments that have not yet been verified.');
+
+define('_PLUGIN_DESC',	'Tools to handle trolls and comment spam. RSS feed of pending comments now included.');
+
+define('_ALLOW',	'[allow]');
+define('_DENY',	'[deny]');
+define('_ALLOWED',	'allowed');
+define('_DENIED',	'denied');
+define('_NO_PERMISSION',	'Sorry. You do not have permission to moderate this comment');
+define('_NOT_AUTH',	'Sorry. not allowed');
+define('_OPT_NOTIFY_EMAIL',	'Email address for pending comment notification (empty to disable)');
+
+?>
